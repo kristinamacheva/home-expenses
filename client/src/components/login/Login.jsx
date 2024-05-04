e482eca5ce8604a8fc2876b1c92f040ac9901fbf
kristinamacheva <macheva.kristina@gmail.com>
2024-05-04 19:02:46 +0300

    Create Login, Register and Logout components

---
+++ client/src/components/login/Login.jsx
@@ -0,0 +1,67 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+} from "@chakra-ui/react";
+
+export default function Login() {
+    return (
+        <Flex
+            minH={"100vh"}
+            align={"center"}
+            justify={"center"}
+            // bg={useColorModeValue("gray.50")}
+            // bg={`linear-gradient(145deg, rgba(66,70,107,1) 6%, rgba(103,111,157,1) 50%, rgba(66,70,107,1) 94%)`}
+            // bg={`linear-gradient(145deg, rgba(45,50,80,1) 6%, rgba(103,111,157,1) 50%, rgba(45,50,80,1) 94%)`}
+            bg={`linear-gradient(135deg, rgba(103,111,157,1) 0%, rgba(135,141,177,1) 25%, rgba(158,162,192,1) 50%, rgba(129,135,174,1) 75%, rgba(103,111,157,1) 100%);`}
+        >
+            <Stack spacing={6} mx={"auto"} maxW={"lg"} py={12} px={6}>
+                <Stack align={"center"}>
+                    <Heading fontSize={"4xl"} color={useColorModeValue("white")}>Домоводител</Heading>
+                </Stack>
+                <Box
+                    rounded={"lg"}
+                    bg={useColorModeValue("white")}
+                    boxShadow={"lg"}
+                    py={12} 
+                    px={{ base: 10, md: 20 }} 
+                >
+                    <Stack spacing={4}>
+                        <Heading fontSize={'2xl'} mb={3} align={"center"}>Влезте в профила си</Heading>
+                        <FormControl id="email">
+                            <FormLabel>Имейл</FormLabel>
+                            <Input type="email" />
+                        </FormControl>
+                        <FormControl id="password">
+                            <FormLabel>Парола</FormLabel>
+                            <Input type="password" />
+                        </FormControl>
+                        <Stack spacing={10}>
+                            <Stack
+                                direction={{ base: "column", sm: "row" }}
+                                align={"start"}
+                                justify={"space-between"}
+                            >
+                                <Checkbox>Запомни ме</Checkbox>
+                                <Text color={"themeBlue.700"}>
+                                    Забравена парола?
+                                </Text>
+                            </Stack>
+                            <Button variant="primary">
+                                Вход
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Flex>
+    );
+}
---
+++ client/src/components/register/Register.jsx
@@ -0,0 +1,102 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    HStack,
+    InputGroup,
+    InputRightElement,
+    Link,
+    useColorModeValue,
+    Icon,
+} from "@chakra-ui/react";
+
+import { FaEye, FaEyeSlash } from "react-icons/fa6";
+
+import { useState } from "react";
+
+export default function Register() {
+    const [showPassword, setShowPassword] = useState(false);
+
+    return (
+        <Flex
+            minH={"100vh"}
+            align={"center"}
+            justify={"center"}
+            bg={`linear-gradient(135deg, rgba(103,111,157,1) 0%, rgba(135,141,177,1) 25%, rgba(158,162,192,1) 50%, rgba(129,135,174,1) 75%, rgba(103,111,157,1) 100%);`}
+        >
+            <Stack spacing={6} mx={"auto"} maxW={"xl"} py={12} px={6}>
+                <Stack align={"center"}>
+                    <Heading fontSize={"4xl"} color={useColorModeValue("white")}>Домоводител</Heading>
+                </Stack>
+                <Box
+                    rounded={"lg"}
+                    bg={useColorModeValue("white")}
+                    boxShadow={"lg"}
+                    py={10} 
+                    px={{ base: 10, md: 20 }} 
+                >
+                    <Stack spacing={4}>
+                    <Heading fontSize={'2xl'} mb={3} align={"center"}>Създайте нов профил</Heading>
+                        <FormControl id="name" isRequired>
+                            <FormLabel>Име</FormLabel>
+                            <Input type="text" />
+                        </FormControl>
+                        <FormControl id="email" isRequired>
+                            <FormLabel>Имейл</FormLabel>
+                            <Input type="email" />
+                        </FormControl>
+                        <FormControl id="phone">
+                            <FormLabel>Телефон</FormLabel>
+                            <Input type="phone" />
+                        </FormControl>
+                        <FormControl id="password" isRequired>
+                            <FormLabel>Парола</FormLabel>
+                            <InputGroup>
+                                <Input
+                                    type={showPassword ? "text" : "password"}
+                                />
+                                <InputRightElement h={"full"}>
+                                    <Button
+                                        bg="transparent"
+                                        variant={"ghost"}
+                                        onClick={() =>
+                                            setShowPassword(
+                                                (showPassword) => !showPassword
+                                            )
+                                        }
+                                    >
+                                        {showPassword ? (
+                                            <Icon as={FaEye} boxSize={3} />
+                                        ) : (
+                                            <Icon as={FaEyeSlash} boxSize={3} />
+                                        )}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
+                        </FormControl>
+                        <Stack spacing={10} pt={4}>
+                            <Button
+                                loadingText="Изпращане"
+                                variant="primary"
+                            >
+                                Регистрация
+                            </Button>
+                        </Stack>
+                        <Stack pt={4}>
+                            <Text align={"center"}>
+                                Вече имате профил?{" "}
+                                <Link color={"themeBlue.700"}>Вход</Link>
+                            </Text>
+                        </Stack>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Flex>
+    );
+}
