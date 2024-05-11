96dd2190cd0593f088131ab5bdb3e03e80b45b9d
kristinamacheva <macheva.kristina@gmail.com>
2024-05-12 01:37:56 +0300

    Create householdManager.js

---
+++ server/src/managers/householdManager.js
@@ -0,0 +1,64 @@
+const households = [
+    {
+        "_id": "1",
+        "name": "Съквартиранти",
+        "members": [
+            { "userId": "1", "role": "Админ" },
+            { "userId": "2", "role": "Член" }
+        ],
+        "admin": { "userId": "1" },
+        "balance": [
+            { "userId": "1", "sum": 60, "type": "+" },
+            { "userId": "2", "sum": 60, "type": "-" }
+        ],
+    },
+    {
+        "_id": "2",
+        "name": "Вкъщи",
+        "members": [
+            { "userId": "1", "role": "Член" },
+            { "userId": "2", "role": "Член" },
+            { "userId": "3", "role": "Админ" }
+        ],
+        "admin": { "userId": "3" },
+        "balance": [
+            { "userId": "1", "sum": 30, "type": "-" },
+            { "userId": "2", "sum": 30, "type": "-" },
+            { "userId": "3", "sum": 60, "type": "+" }
+        ],
+    },
+    {
+        "_id": "3",
+        "name": "Обмен",
+        "members": [
+            { "userId": "1", "role": "Член" },
+            { "userId": "2", "role": "Админ" }
+        ],
+        "admin": { "userId": "2" },
+        "balance": [
+            { "userId": "1", "sum": 0, "type": "+" },
+            { "userId": "2", "sum": 0, "type": "+" }
+        ],
+    }
+]
+
+exports.getAll = () => households.slice();
+
+exports.create = (householdData) => {
+    const balance = [householdData.admin, ...householdData.members].map(user => ({
+        userId: user.userId,
+        sum: 0,
+        type: "+"
+    }));
+
+    const newHousehold = {
+        _id: `${households.length + 1}`,
+        //createdAt...
+        ...householdData,
+        balance,
+    };
+
+    households.push(newHousehold);
+
+    return newHousehold;
+};
