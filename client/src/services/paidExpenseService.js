afb1600f50b24dec6da0c521a07249439cb863ec
kristinamacheva <macheva.kristina@gmail.com>
2024-06-18 13:43:25 +0300

    Update paidExpenseService.js

--- client/src/services/paidExpenseService.js
+++ client/src/services/paidExpenseService.js
@@ -8,3 +8,37 @@ export const getAll = async (householdId) => {
     return result;
 };
 
+// export const getOne = async (householdId) => {
+//     try {
+//         const result = await request.get(`${baseUrl}/${householdId}`);
+//         return result;
+//     } catch (error) {
+//         console.error('Error fetching household data:', error);
+//         return null; 
+//     }
+// }
+
+// export const getOneReducedData = async (householdId) => {
+//     try {
+//         const result = await request.get(`${baseUrl}/${householdId}/reduced`);
+//         return result;
+//     } catch (error) {
+//         console.error('Error fetching household data:', error);
+//         return null; 
+//     }
+// }
+
+// export const create = async (householdData) => {
+//     const result = await request.post(baseUrl, householdData);
+    
+//     return result;
+// };
+
+// export const edit = async (householdId, householdData) => {
+//     const result = await request.put(`${baseUrl}/${householdId}`, householdData);
+
+//     return result;
+// };
+
+// export const remove = async (householdId) => request.remove(`${baseUrl}/${householdId}`);
+
