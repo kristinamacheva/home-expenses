6d8017705902927425023ef86753b4d27af2a5bb
kristinamacheva <macheva.kristina@gmail.com>
2024-06-22 00:11:12 +0300

    Create an array of objects with only `id` and `sum` properties

--- client/src/hooks/useEqualSplit.js
+++ client/src/hooks/useEqualSplit.js
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react";
 
-export default function useEqualSplit(totalAmount, members) {
+export default function useEqualSplit(totalAmount, members, onUpdate) {
     const [equalSplit, setEqualSplit] = useState([]);
 
     const calculateEqualSplit = () => {
         const totalAmountInSubunits = Math.round(Number(totalAmount) * 100);
         const numberOfPeople = members.length;
 
-        if (numberOfPeople === 0) {
-            setEqualSplit([]);
-            return;
-        }
+        // if (numberOfPeople === 0) {
+        //     setEqualSplit([]);
+        //     return;
+        // }
 
         const amountPerPersonInCents = Math.floor(
             totalAmountInSubunits / numberOfPeople
@@ -20,13 +20,19 @@ export default function useEqualSplit(totalAmount, members) {
         const updatedMembers = members.map((member) => ({
             ...member,
             sum: (
-                (amountPerPersonInCents +
-                    (remainderInCents-- > 0 ? 1 : 0)) /
+                (amountPerPersonInCents + (remainderInCents-- > 0 ? 1 : 0)) /
                 100
             ).toFixed(2),
         }));
 
+        // Create an array of objects with only `id` and `sum` properties
+        const simplifiedMembers = updatedMembers.map((member) => ({
+            _id: member._id,
+            sum: member.sum,
+        }));
+
         setEqualSplit(updatedMembers);
+        onUpdate(simplifiedMembers);
     };
 
     useEffect(() => {
