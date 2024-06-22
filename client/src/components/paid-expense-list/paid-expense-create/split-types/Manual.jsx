03ef56f27931a180c5f51b22efb8938b3ad01964
kristinamacheva <macheva.kristina@gmail.com>
2024-06-22 17:27:48 +0300

    Change layout and way of displaying sums

--- client/src/components/paid-expense-list/paid-expense-create/split-types/Manual.jsx
+++ client/src/components/paid-expense-list/paid-expense-create/split-types/Manual.jsx
@@ -49,47 +49,50 @@ export default function Manual({
     // TODO: update state only when the amounts are correct - if not - error
     const handleMessageChange = (newAmounts) => {
         const totalEnteredInCents = newAmounts.reduce(
-            (acc, member) => acc + parseFloat(member.sum) * 100,
+            (acc, member) => acc + Number(member.sum) * 100,
             0
         );
-    
+
         const totalAmountInCents = amount * 100;
-    
+
         if (totalEnteredInCents > totalAmountInCents) {
             setMessageColor("red.400");
             setMessage(
-                `Сборът от сумите надвишава сумата на разхода с ${
-                    ((totalEnteredInCents - totalAmountInCents) / 100).toFixed(2)
-                } лв.`
+                `Сборът от сумите надвишава сумата на разхода с ${(
+                    (totalEnteredInCents - totalAmountInCents) /
+                    100
+                ).toFixed(2)} лв.`
             );
         } else if (totalEnteredInCents < totalAmountInCents) {
             setMessageColor("red.400");
             setMessage(
-                `Трябва да доплатите още ${((totalAmountInCents - totalEnteredInCents) / 100).toFixed(2)} лв.`
+                `Трябва да доплатите още ${(
+                    (totalAmountInCents - totalEnteredInCents) /
+                    100
+                ).toFixed(2)} лв.`
             );
         } else {
             setMessageColor("green.400");
-            setMessage("Сборът от сумите е равен на сумата на разхода");
+            setMessage("Сборът от сумите е равен на сумата на разхода.");
         }
-    
+
         onUpdate(newAmounts, message);
     };
-    
+
     const handleChange = (id, value) => {
         // Restricting input to 2 decimal places
         const regex = /^\d*([\,\.]?\d{0,2})?$/;
-        // const regex = /^\d*([\,\.]?\d{0,2})?$/;
         // TODO: . issue
-        
+
         if (!regex.test(value)) {
             // If input does not match the regex, do not update state
             return;
         }
 
         const newAmounts = manualAmounts.map((entry) =>
-            entry._id === id ? { ...entry, sum: parseFloat(value) || 0 } : entry
+            entry._id === id ? { ...entry, sum: Number(value) || 0 } : entry
         );
-        
+
         setManualAmounts(newAmounts);
         handleMessageChange(newAmounts);
     };
@@ -110,111 +113,109 @@ export default function Manual({
     };
 
     return (
-        <Flex
-            wrap={{ base: "nowrap", lg: "wrap" }}
-            direction={{ base: "column", lg: "row" }}
-            justifyContent="space-between"
-        >
-            {splitManualMembers.map((member) => (
-                <Stack width={{ base: "100%", lg: "48%" }} spacing="0.5">
-                    <Card
-                        key={member._id}
-                        p="4"
-                        display="flex"
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="space-between"
-                        mb={{ base: "2", lg: "3" }}
+        <Stack>
+            <Flex
+                wrap={{ base: "nowrap", lg: "wrap" }}
+                direction={{ base: "column", lg: "row" }}
+                justifyContent="space-between"
+            >
+                {splitManualMembers.map((member) => (
+                    <Stack
+                        width={{ base: "100%", lg: "48%" }}
+                        spacing="0.5"
+                        mt={2}
                     >
-                        <Stack
+                        <Card
+                            key={member._id}
+                            p="4"
                             display="flex"
-                            alignItems="center"
                             direction="row"
-                            mr="2"
+                            alignItems="center"
+                            justifyContent="space-between"
+                            mb={{ base: "2", lg: "3" }}
                         >
-                            <Avatar
-                                name={member.name}
-                                // src={avatar || ""}
-                                background={"themeYellow.900"}
-                                mr="3"
+                            <Stack
+                                display="flex"
+                                alignItems="center"
+                                direction="row"
+                                mr="2"
+                            >
+                                <Avatar
+                                    name={member.name}
+                                    // src={avatar || ""}
+                                    background={"themeYellow.900"}
+                                    mr="3"
+                                />
+                                <Text>{member.name}</Text>
+                            </Stack>
+                            <Stack
+                                display="flex"
+                                alignItems="center"
+                                direction="row"
+                            >
+                                <Text mr="1">
+                                    {(
+                                        manualAmounts.find(
+                                            (entry) => entry._id === member._id
+                                        )?.sum || 0
+                                    ).toFixed(2) + " лв."}
+                                </Text>
+                                {splitManualMembers.length > 2 &&
+                                    ((member._id !== userId && (
+                                        <IconButton
+                                            aria-label="Изтрийте"
+                                            title="Изтрийте"
+                                            icon={
+                                                <FaRegTrashCan fontSize="20px" />
+                                            }
+                                            variant="ghost"
+                                            color="themePurple.800"
+                                            onClick={() =>
+                                                onMemberRemove(member._id)
+                                            }
+                                        />
+                                    )) ||
+                                        (member._id === userId &&
+                                            showCreatorDeleteButton && (
+                                                <IconButton
+                                                    aria-label="Изтрийте"
+                                                    title="Изтрийте"
+                                                    icon={
+                                                        <FaRegTrashCan fontSize="20px" />
+                                                    }
+                                                    variant="ghost"
+                                                    color="themePurple.800"
+                                                    onClick={() =>
+                                                        onMemberRemove(
+                                                            member._id
+                                                        )
+                                                    }
+                                                />
+                                            )))}
+                            </Stack>
+                        </Card>
+                        <InputGroup>
+                            <InputLeftAddon>Сума</InputLeftAddon>
+                            <Input
+                                type="number"
+                                value={
+                                    manualAmounts.find(
+                                        (entry) => entry._id === member._id
+                                    )?.sum || 0
+                                }
+                                onChange={(e) =>
+                                    handleChange(member._id, e.target.value)
+                                }
                             />
-                            <Text>{member.name}</Text>
-                        </Stack>
-                        <Stack
-                            display="flex"
-                            alignItems="center"
-                            direction="row"
-                        >
-                            <Text mr="1">
-                                {manualAmounts.find(
-                                    (entry) => entry._id === member._id
-                                )?.sum || 0}{" "}
-                                лв.
-                            </Text>
-                            {splitManualMembers.length > 2 &&
-                                ((member._id !== userId && (
-                                    <IconButton
-                                        aria-label="Изтрийте"
-                                        title="Изтрийте"
-                                        icon={<FaRegTrashCan fontSize="20px" />}
-                                        variant="ghost"
-                                        color="themePurple.800"
-                                        onClick={() =>
-                                            onMemberRemove(member._id)
-                                        }
-                                    />
-                                )) ||
-                                    (member._id === userId &&
-                                        showCreatorDeleteButton && (
-                                            <IconButton
-                                                aria-label="Изтрийте"
-                                                title="Изтрийте"
-                                                icon={
-                                                    <FaRegTrashCan fontSize="20px" />
-                                                }
-                                                variant="ghost"
-                                                color="themePurple.800"
-                                                onClick={() =>
-                                                    onMemberRemove(member._id)
-                                                }
-                                            />
-                                        )))}
-                            {/* {splitManualMembers.length > 2 &&
-                                (member._id === userId && showCreatorDeleteButton) && (
-                                    <IconButton
-                                        aria-label="Изтрийте"
-                                        title="Изтрийте"
-                                        icon={<FaRegTrashCan fontSize="20px" />}
-                                        variant="ghost"
-                                        color="themePurple.800"
-                                        onClick={() =>
-                                            onMemberRemove(member._id)
-                                        }
-                                    />
-                                )} */}
-                        </Stack>
-                    </Card>
-                    <InputGroup>
-                        <InputLeftAddon>Сума</InputLeftAddon>
-                        <Input
-                            type="number"
-                            value={
-                                manualAmounts.find(
-                                    (entry) => entry._id === member._id
-                                )?.sum || 0
-                            }
-                            onChange={(e) =>
-                                handleChange(member._id, e.target.value)
-                            }
-                        />
-                    </InputGroup>
-                </Stack>
-            ))}
+                        </InputGroup>
+                    </Stack>
+                ))}
+            </Flex>
             {message && (
                 <Text mt="2" color={messageColor}>
                     {message}
                 </Text>
             )}
-        </Flex>
+        </Stack>
     );
 }
