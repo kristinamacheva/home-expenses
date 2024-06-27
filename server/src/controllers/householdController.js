977528c4bb610bca1e5f585421339afbf3b8740a
kristinamacheva <macheva.kristina@gmail.com>
2024-06-27 22:17:20 +0300

    Improve error handling

--- server/src/controllers/householdController.js
+++ server/src/controllers/householdController.js
@@ -1,67 +1,53 @@
-const router = require('express').Router();
-const householdManager = require('../managers/householdManager');
-const getHousehold = require('../middlewares/householdMiddleware');
-const paidExpenseController = require('./paidExpenseController');
-const { isAuth } = require('../middlewares/authMiddleware');
-const getPaidExpense = require('../middlewares/paidExpenseMiddleware');
+const router = require("express").Router();
+const householdManager = require("../managers/householdManager");
+const getHousehold = require("../middlewares/householdMiddleware");
+const paidExpenseController = require("./paidExpenseController");
+const { isAuth } = require("../middlewares/authMiddleware");
+const getPaidExpense = require("../middlewares/paidExpenseMiddleware");
 const { AppError } = require("../utils/AppError");
 
-router.get('/', async (req, res, next) => {
-    const userId = req.userId
-    
-    try {
-        const households = await householdManager.getAllWithUsers(userId);
-        res.status(200).json(households);
-    } catch (error) {
-        next(error);
-    }
-});
-
-router.post('/', async (req, res) => {
-    const {
-        name,
-        members,
-    } = req.body;
+router.post("/", async (req, res, next) => {
+    const { name, members } = req.body;
 
     try {
         const newHousehold = await householdManager.create({
             name,
             members,
             admin: req.userId,
-            // admin: '664f630fb14becfeb98d2e1f',
         });
 
         res.status(201).json(newHousehold);
-    } catch (err) {
-        console.log(err);
-        res.status(400).json({
-            message: 'Cannot create household'
-        });
+    } catch (error) {
+        next(error);
     }
 });
 
 // Middleware to get householdId
-router.use('/:householdId', getHousehold);
+router.use("/:householdId", getHousehold);
 
-router.get('/:householdId', async (req, res) => {
-    // TODO: lean?
+router.get("/:householdId", async (req, res, next) => {
     const householdId = req.householdId;
 
-    const household = await householdManager.getOneWithUsersAndBalance(householdId).lean();
-    res.json(household);
+    try {
+        const household = await householdManager.getOneWithMembers(householdId);
+
+        res.status(200).json(household);
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.get('/:householdId/members', async (req, res, next) => {
+router.get("/:householdId/members", async (req, res, next) => {
     const { role, details } = req.query;
     let users;
     const householdId = req.householdId;
 
     try {
-        if (details === 'true') {
+        if (details === "true") {
             users = await householdManager.getOneMembersDetails(householdId);
-        } else if (role === 'child') {
+        } else if (role === "child") {
             users = await householdManager.getOneChildMembers(householdId);
-        } else if (role === 'not-child') {
+        } else if (role === "not-child") {
             users = await householdManager.getOneNonChildMembers(householdId);
         } else {
             users = await householdManager.getOneMembers(householdId);
@@ -73,66 +59,67 @@ router.get('/:householdId/members', async (req, res, next) => {
     }
 });
 
-router.get('/:householdId/balances', async (req, res) => {
-    const { details } = req.query;
-    let balances;
+router.get("/:householdId/balances", async (req, res, next) => {
     const householdId = req.householdId;
 
     try {
-        if (details === 'true') {
-            balances = await householdManager.getAllBalancesDetails(householdId);
-        } else {
-            balances = await householdManager.getAllBalances(householdId);
-        }
+        const balances = await householdManager.getOneBalances(householdId);
 
-        res.json(balances);
+        res.status(200).json(balances);
     } catch (error) {
-        res.status(500).json({ error: 'An error occurred while fetching balances' });
+        next(error);
     }
 });
 
-router.get('/:householdId/reduced', async (req, res) => {
+// // TODO: update status
+// router.put("/:householdId", async (req, res) => {
+//     const householdId = req.householdId;
+
+//     const household = await householdManager.update(householdId, req.body);
+//     res.json(household);
+// });
+
+router.put("/:householdId/leave", async (req, res, next) => {
+    const userId = req.userId;
     const householdId = req.householdId;
 
-    const household = await householdManager.getOneReducedData(householdId);
-    res.json(household);
-});
-
-// TODO: update status
-router.put('/:householdId', async (req, res) => {
-    const householdId = req.householdId;
-
-    const household = await householdManager.update(householdId, req.body);
-    res.json(household);
-});
-
-router.delete('/:householdId', async (req, res) => {
-    const householdId = req.householdId;
-
-    await householdManager.delete(householdId);
-    // TODO: result
-    res.status(204).end();
-});
-
-router.put('/:householdId/leave', async (req, res) => {
     try {
-        const userId = req.userId;
-        const householdId = req.householdId;
-        const result = await householdManager.leave(userId, householdId);
-        res.status(200).json({
-            message: 'Successfully left the household',
-        });
+        await householdManager.leave(userId, householdId);
+
+        res.status(204).end();
     } catch (error) {
-        if (error.statusCode) {
-            res.status(error.statusCode).json({ message: error.message });
-        } else {
-            // Fallback to a generic server error
-            console.error('Error in leaving household:', error);
-            res.status(500).json({ message: 'Internal Server Error' });
-        }
+        next(error);
     }
 });
 
-router.use('/:householdId/paidExpenses', paidExpenseController);
+router.use("/:householdId/paidExpenses", paidExpenseController);
 
 module.exports = router;
+
+
+// router.get('/', async (req, res, next) => {
+//     const userId = req.userId
+
+//     try {
+//         const households = await householdManager.getAllWithUsers(userId);
+//         res.status(200).json(households);
+//     } catch (error) {
+//         next(error);
+//     }
+// });
+
+// TODO
+// router.delete('/:householdId', async (req, res) => {
+//     const householdId = req.householdId;
+
+//     await householdManager.delete(householdId);
+//     // TODO: result
+//     res.status(204).end();
+// });
+
+// router.get('/:householdId/reduced', async (req, res) => {
+//     const householdId = req.householdId;
+
+//     const household = await householdManager.getOneReducedData(householdId);
+//     res.json(household);
+// });
--- server/src/managers/householdManager.js
+++ server/src/managers/householdManager.js
@@ -4,27 +4,45 @@ const User = require("../models/User");
 const mongoose = require("mongoose");
 const { AppError } = require("../utils/AppError");
 
-// TODO: send different response if the resourse doesnt exist or is not found
-// TODO: filter with search params in the db
-exports.getAll = () => Household.find();
+exports.getOneWithMembers = async (householdId) => {
+    const aggregationPipeline = [
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(householdId) // Match the household by its ObjectId
+            }
+        },
+        {
+            $lookup: {
+                from: "users", // Collection to join for members
+                localField: "members.user",
+                foreignField: "_id",
+                as: "members"
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                name: 1,
+                members: {
+                    $map: {
+                        input: "$members",
+                        as: "member",
+                        in: {
+                            _id: "$$member._id",
+                            name: "$$member.name",
+                            avatar: "$$member.avatar",
+                            avatarColor: "$$member.avatarColor"
+                        }
+                    }
+                }
+            }
+        }
+    ];
 
-exports.getAllWithUsers = async (userId) => {
-    const result = await Household.find({ "members.user": userId })
-        .populate("members.user", "_id name email phone")
-        .populate("balance.user", "_id name")
-        .lean();
-
-    return result;
+    const household = await Household.aggregate(aggregationPipeline);
+    return household[0]; // Return the first result (assuming householdId is unique)
 };
 
-exports.getOne = (householdId) => Household.findById(householdId);
-
-exports.getOneWithUsersAndBalance = (householdId) =>
-    this.getOne(householdId)
-        .populate("members.user", "_id name email phone")
-        // .populate("admins", "name")
-        .populate("balance.user", "_id name");
-// .populate('balance.user', '-_id name');
 exports.getOneMembers = async (householdId) => {
     const pipeline = [
         { $match: { _id: new mongoose.Types.ObjectId(householdId) } },
@@ -42,6 +60,7 @@ exports.getOneMembers = async (householdId) => {
             $project: {
                 _id: "$userDetails._id",
                 name: "$userDetails.name",
+                email: "$userDetails.email",
             },
         },
     ];
@@ -104,22 +123,6 @@ exports.getOneMembersDetails = async (householdId) => {
         return allMembers[0].members;
     }
     return [];
-};
-
-exports.getAllBalances = async (householdId) => {
-    const allBalances = await Household.findById(householdId)
-        .populate("balance.user", "_id name")
-        .select("balance");
-
-    return allBalances;
-};
-
-exports.getAllBalancesDetails = async (householdId) => {
-    const allMembersDetails = await Household.findById(householdId)
-        .populate("balance.user", "_id name avatar avatarColor")
-        .select("balance");
-
-    return allMembersDetails;
 };
 
 exports.getOneNonChildMembers = async (householdId) => {
@@ -226,95 +229,234 @@ exports.getOneChildMembers = async (householdId) => {
     return [];
 };
 
-// TODO: Send email instead of id?
-exports.getOneReducedData = (householdId) => {
-    const household = this.getOne(householdId).populate(
-        "members.user",
-        "_id name email phone"
-    );
-    // if (!household) return null;
+exports.getOneBalances = async (householdId) => {
+    const allBalances = await Household.aggregate([
+        { $match: { _id: new mongoose.Types.ObjectId(householdId) } }, // Match the household by ID
+        { $unwind: "$balance" }, // Deconstruct the balance array
+        {
+            $lookup: {
+                from: "users", // Perform a join with the users collection
+                localField: "balance.user", // Match user field in balance
+                foreignField: "_id", // with _id field in users collection
+                as: "userDetails", // Output to userDetails array
+            },
+        },
+        { $unwind: "$userDetails" }, // Deconstruct the userDetails array
+        {
+            $project: {
+                _id: 0, // Exclude the default _id field from the output
+                "balance._id": "$userDetails._id", // Include user _id in balance
+                "balance.name": "$userDetails.name", // Include user name in balance
+                "balance.avatar": "$userDetails.avatar", // Include user avatar in balance
+                "balance.avatarColor": "$userDetails.avatarColor", // Include user avatarColor in balance
+                "balance.type": "$balance.type", // Include balance type
+                "balance.sum": "$balance.sum", // Include balance sum
+            },
+        },
+        {
+            $group: {
+                _id: "$_id", // Group by household ID (can be null since we're only matching one household)
+                balance: {
+                    $push: {
+                        _id: "$balance._id",
+                        name: "$balance.name",
+                        avatar: "$balance.avatar",
+                        avatarColor: "$balance.avatarColor",
+                        type: "$balance.type",
+                        sum: "$balance.sum",
+                    },
+                },
+            },
+        },
+        {
+            $project: {
+                _id: 0, // Exclude the grouping key from the output
+                balance: 1, // Include the balance array
+            },
+        },
+    ]);
 
-    const { name, members } = household;
-
-    return { name, members };
+    if (allBalances.length > 0) {
+        return allBalances[0].balance;
+    }
+    return [];
 };
 
 exports.create = async (householdData) => {
-    try {
-        const { name, members, admin } = householdData;
+    const { name, members, admin } = householdData;
 
-        // Fetch the admin user by ID
-        const adminUser = await User.findById(admin);
-        if (!adminUser) {
-            throw new Error(`Admin user not found`);
+    // Fetch the admin user by ID
+    const adminUser = await User.findById(admin);
+    if (!adminUser) {
+        throw new AppError("Админът не е намерен", 401);
+    }
+
+    // Check for duplicate emails in members
+    const uniqueEmails = new Set();
+    for (const member of members) {
+        if (uniqueEmails.has(member.email)) {
+            throw new AppError(
+                `Имейлът се среща повече от 1 път: ${member.email}`,
+                400
+            );
         }
+        uniqueEmails.add(member.email);
+    }
 
-        // Check for duplicate emails in members
-        const uniqueEmails = new Set();
-        for (const member of members) {
-            if (uniqueEmails.has(member.email)) {
-                throw new Error(`Duplicate email found: ${member.email}`);
-            }
-            uniqueEmails.add(member.email);
-        }
+    // Fetch member users by their emails
+    const memberUsers = await User.find({
+        email: { $in: Array.from(uniqueEmails) },
+    }).select("_id email");
 
-        // Fetch member users by their emails
-        // const memberEmails = members.map((member) => member.email);
-        const memberUsers = await User.find({
-            email: { $in: Array.from(uniqueEmails) },
-        }).select("_id email");
-        // console.log(memberUsers);
+    if (memberUsers.length !== uniqueEmails.size) {
+        throw new AppError("1 или повече имейла не бяха намерени", 400);
+    }
 
-        if (memberUsers.length !== uniqueEmails.size) {
-            throw new Error("Some member emails not found");
-        }
+    // Create the new household
+    const newHousehold = new Household({
+        name,
+        members: [
+            {
+                user: adminUser._id,
+                role: "Админ",
+            },
+        ],
+        admins: [adminUser._id],
+        balance: [
+            {
+                user: adminUser._id,
+                sum: 0, // Default sum
+                type: "+", // Default type
+            },
+        ],
+    });
 
-        // Create the new household
-        const newHousehold = new Household({
-            name,
-            members: [
-                {
-                    user: adminUser._id,
-                    role: "Админ",
-                },
-            ],
-            admins: [adminUser._id],
-            balance: [
-                {
-                    user: adminUser._id,
-                    sum: 0, // Default sum
-                    type: "+", // Default type
-                },
-            ],
+    // Save the household to the database
+    await newHousehold.save();
+
+    // Add the household ID to the admin user's households array
+    adminUser.households.push(newHousehold._id);
+    await adminUser.save();
+
+    //TODO: check if user is already a member
+    // Create invitations for each member
+    for (const member of members) {
+        const currentUser = memberUsers.find(
+            (user) => user.email === member.email
+        );
+
+        const invitation = new HouseholdInvitation({
+            user: currentUser._id,
+            household: newHousehold._id,
+            role: member.role,
+            creator: adminUser._id,
         });
 
-        // Save the household to the database
-        await newHousehold.save();
-
-        //TODO: check if user is already a member
-        // Create invitations for each member
-        for (const member of members) {
-            const currentUser = memberUsers.find(
-                (user) => user.email === member.email
-            );
-
-            const invitation = new HouseholdInvitation({
-                user: currentUser._id,
-                household: newHousehold._id,
-                role: member.role,
-                creator: adminUser._id,
-            });
-
-            await invitation.save();
-        }
-
-        console.log("Household created:", newHousehold);
-        return newHousehold;
-    } catch (error) {
-        console.error("Error creating household:", error);
-        throw error;
+        await invitation.save();
     }
+
+    return newHousehold;
 };
+
+exports.leave = async (userId, householdId) => {
+    const household = await Household.findById(householdId);
+
+    // Check if the user is a member of the household
+    const userIndex = household.members.findIndex(
+        (member) => member.user.toString() === userId
+    );
+    if (userIndex === -1) {
+        throw new AppError("Потребителят не е член на домакинството.", 401);
+    }
+
+    // Check if the user is an admin (using admins array)
+    const isAdmin = household.admins.some(
+        (admin) => admin.toString() === userId
+    );
+    if (isAdmin) {
+        // If the user is an admin, check if they are the last admin
+        if (household.admins.length === 1) {
+            throw new AppError(
+                "Вие сте единственият админ на домакинството и не може да го напуснете.",
+                403
+            );
+        }
+    }
+
+    // Check if the user's role is not "Дете"
+    const userRole = household.members[userIndex].role;
+    if (userRole !== "Дете") {
+        // Check if the user has a non-zero balance sum
+        const userBalance = household.balance.find(
+            (entry) => entry.user.toString() === userId
+        );
+        if (userBalance && userBalance.sum !== 0) {
+            throw new AppError(
+                "Не може да напуснете домакинството, ако балансът ви не е 0.",
+                403
+            );
+        }
+    }
+
+    // Remove the user from the members array
+    household.members.splice(userIndex, 1);
+
+    // Save the updated household
+    await household.save();
+
+    // TODO:transaction
+    // Remove the household from the user's households array
+    await User.findByIdAndUpdate(userId, {
+        $pull: { households: householdId },
+    });
+};
+
+// TODO: send different response if the resourse doesnt exist or is not found
+// TODO: filter with search params in the db
+// exports.getAll = () => Household.find();
+
+// exports.getAllWithUsers = async (userId) => {
+//     const result = await Household.find({ "members.user": userId })
+//         .populate("members.user", "_id name email phone")
+//         .populate("balance.user", "_id name")
+//         .lean();
+
+//     return result;
+// };
+
+// exports.getOne = (householdId) => Household.findById(householdId);
+
+// exports.getOneWithMembersAndBalance = (householdId) =>
+//     Household.findById(householdId)
+//         .populate("members.user", "_id name avatar avatarColor")
+//         .populate("balance.user", "_id name avatar avatarColor");
+
+// exports.getAllBalancesDetails = async (householdId) => {
+//     const allMembersDetails = await Household.findById(householdId)
+//         .populate("balance.user", "_id name avatar avatarColor")
+//         .select("balance");
+
+//     return allMembersDetails;
+// };
+
+// // TODO: Send email instead of id?
+// exports.getOneReducedData = (householdId) => {
+//     const household = this.getOne(householdId).populate(
+//         "members.user",
+//         "_id name email phone"
+//     );
+//     // if (!household) return null;
+
+//     const { name, members } = household;
+
+//     return { name, members };
+// };
+
+// // TODO: Validation, users
+// exports.update = (householdId, householdData) =>
+//     Household.findByIdAndUpdate(householdId, householdData);
+
+// exports.delete = (householdId) => Household.findByIdAndDelete(householdId);
 
 // exports.create = async (householdData) => {
 //     try {
@@ -384,61 +526,3 @@ exports.create = async (householdData) => {
 //         throw error;
 //     }
 // };
-
-// TODO: fix error status
-exports.leave = async (userId, householdId) => {
-    try {
-        const household = await Household.findById(householdId);
-
-        // Check if the user is a member of the household
-        const userIndex = household.members.findIndex(
-            (member) => member.user.toString() === userId
-        );
-        if (userIndex === -1) {
-            const error = {
-                message: "Потребителят не е член на домакинството",
-                statusCode: 404 
-            };
-            throw error;
-        }
-
-        // Check if the user is an admin (using admins array)
-        const isAdmin = household.admins.some(admin => admin.toString() === userId);
-        if (isAdmin) {
-            // If the user is an admin, check if they are the last admin
-            if (household.admins.length === 1) {
-                const error = new Error("You are the only admin in the household and cannot leave");
-                error.statusCode = 403; 
-                throw error;
-            }
-        }
-
-        // Check if the user's role is not "Дете"
-        const userRole = household.members[userIndex].role;
-        if (userRole !== 'Дете') {
-            // Check if the user has a non-zero balance sum
-            const userBalance = household.balance.find(
-                (entry) => entry.user.toString() === userId
-            );
-            if (userBalance && userBalance.sum !== 0) {
-                throw new Error("User cannot leave the household with a non-zero balance");
-            }
-        }
-
-        // Remove the user from the members array
-        household.members.splice(userIndex, 1);
-
-        // Save the updated household
-        await household.save();
-
-        return { message: "Successfully left the household" };
-    } catch (error) {
-        throw error;
-    }
-};
-
-// TODO: Validation, users
-exports.update = (householdId, householdData) =>
-    Household.findByIdAndUpdate(householdId, householdData);
-
-exports.delete = (householdId) => Household.findByIdAndDelete(householdId);
