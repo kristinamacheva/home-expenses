d469241ccff73b12ae2f69ca5c4891d6a1985be5
kristinamacheva <macheva.kristina@gmail.com>
2024-07-03 16:58:53 +0300

    Integrate socket.ioi with improved CORS handling and initialization

--- server/src/config/expressConfig.js
+++ server/src/config/expressConfig.js
@@ -1,18 +1,43 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const { auth } = require('../middlewares/authMiddleware');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { auth } = require("../middlewares/authMiddleware");
+const http = require("http"); // Import http module for socket.io
+const socketAuthMiddleware = require("../middlewares/socketAuthMiddleware");
+const { initializeSocket } = require("./socket");
 
 function expressConfig(app) {
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
-    // app.use(cors());
-    app.use(cors({
-        origin: 'http://localhost:5173', 
-        credentials: true
-    }));
+
+    // Define allowed origins
+    const allowedOrigins = [
+        "http://localhost:5173",
+        "http://localhost:4173",
+        "http://192.168.56.1:4173"
+    ];
+
+    // CORS configuration
+    app.use(
+        cors({
+            origin: function (origin, callback) {
+                // Check if the origin is in the list of allowed origins
+                if (!origin || allowedOrigins.includes(origin)) {
+                    callback(null, true); // Allow the request
+                } else {
+                    callback(new Error("Not allowed by CORS")); // Block the request
+                }
+            },
+            credentials: true, // Allow credentials such as cookies, authorization headers
+        })
+    );
+
     app.use(cookieParser());
     app.use(auth);
+
+    const server = initializeSocket(app);
+
+    app.set("server", server);
 }
 
 module.exports = expressConfig;
