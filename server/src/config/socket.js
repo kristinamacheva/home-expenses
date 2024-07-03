6cb086f89fc8a0ffe3a0605b2812003b68043085
kristinamacheva <macheva.kristina@gmail.com>
2024-07-03 16:52:29 +0300

    Socket.io configuration

---
+++ server/src/config/socket.js
@@ -0,0 +1,77 @@
+const socketIO = require('socket.io');
+const http = require('http');
+const socketAuthMiddleware = require("../middlewares/socketAuthMiddleware");
+
+let io;
+
+function initializeSocket(app) {
+    const server = http.createServer(app);
+    io = socketIO(server, {
+        cors: {
+            origin: ["http://localhost:5173", "http://localhost:4173", "http://192.168.56.1:4173"],
+            methods: ["GET", "POST"],
+            credentials: true
+        }
+    });
+
+    io.use(socketAuthMiddleware);
+
+    io.on("connection", (socket) => {
+        console.log(`User connected: ${socket.userId}`);
+
+        // Store connected sockets by userId
+        // global object io.userSockets is used to keep track of active connections
+        if (!io.userSockets) {
+            io.userSockets = {};
+        }
+
+        // If there are no active connections for a user, an array is created to hold their connections
+        if (!io.userSockets[socket.userId]) {
+            io.userSockets[socket.userId] = [];
+        }
+        
+        io.userSockets[socket.userId].push(socket);
+
+        // console.log(io.userSockets);
+
+        socket.on("disconnect", () => {
+            console.log(`User disconnected: ${socket.userId}`);
+            io.userSockets[socket.userId] = io.userSockets[socket.userId].filter(s => s !== socket);
+            if (io.userSockets[socket.userId].length === 0) {
+                delete io.userSockets[socket.userId];
+            }
+        });
+    });
+
+    app.set("io", io);
+    app.set("server", server);
+
+    return server;
+}
+
+function getIO() {
+    if (!io) {
+        throw new Error("Socket.io not initialized!");
+    }
+    return io;
+}
+
+function sendNotificationToUser(userId, notification) {
+    if (!io.userSockets) {
+        console.log('No active socket connections.');
+        return;
+    }
+
+    const sockets = io.userSockets[userId];
+    if (!sockets || sockets.length === 0) {
+        console.log(`No active socket connections for user ${userId}.`);
+        return;
+    }
+
+    sockets.forEach(socket => {
+        socket.emit("notification", notification);
+    });
+}
+
+
+module.exports = { initializeSocket, getIO, sendNotificationToUser };
--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const dbConnect = require('./config/dbConfig');
-
 const expressConfig = require('./config/expressConfig');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/ErrorHandlerMiddleware');
 
 const app = express();
-
 const PORT = 5000;
 
 expressConfig(app);
@@ -21,4 +19,7 @@ app.use(routes);
 // Error handling middleware (Last middleware to use)
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+// Retrieve server and io instances from app settings
+const server = app.get('server');
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
