c7978c85e81118fc043c4604d5da32562b294350
kristinamacheva <macheva.kristina@gmail.com>
2024-07-03 16:53:44 +0300

    Implement socket authentication middleware for handling socket.io connections

---
+++ server/src/middlewares/socketAuthMiddleware.js
@@ -0,0 +1,34 @@
+const cookie = require("cookie");
+const jwt = require("../lib/jwt");
+const User = require("../models/User");
+const mongoose = require("mongoose");
+
+// invoked only when a client attempts to establish a socket connection
+const socketAuthMiddleware = async (socket, next) => {
+    try {
+        const cookies = cookie.parse(socket.handshake.headers.cookie || "");
+        const token = cookies["auth"];
+
+        if (!token) {
+            return next(new Error("Authentication error: Token not found"));
+        }
+
+        const decodedToken = await jwt.verify(token, process.env.AUTH_ACCESS_TOKEN_SECRET);
+
+        if (!mongoose.Types.ObjectId.isValid(decodedToken._id)) {
+            return next(new Error("Authentication error: Invalid user ID format"));
+        }
+
+        const user = await User.findById(decodedToken._id);
+        if (!user) {
+            return next(new Error("Authentication error: User not found"));
+        }
+
+        socket.userId = decodedToken._id; // Attach user ID to socket object
+        next(); // Allow the connection
+    } catch (err) {
+        next(new Error("Authentication error: " + err.message));
+    }
+};
+
+module.exports = socketAuthMiddleware;
