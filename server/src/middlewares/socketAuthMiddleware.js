85ab25d275c4cf63dae88e5630d4f2db95ea52e7
kristinamacheva <macheva.kristina@gmail.com>
2024-07-10 23:18:37 +0300

    Add comments to explain socket.io middleware

--- server/src/middlewares/socketAuthMiddleware.js
+++ server/src/middlewares/socketAuthMiddleware.js
@@ -27,6 +27,8 @@ const socketAuthMiddleware = async (socket, next) => {
         socket.userId = decodedToken._id; // Attach user ID to socket object
         next(); // Allow the connection
     } catch (err) {
+        // When Socket.io receives an error from the middleware, it will terminate the connection 
+        // attempt and send an error message back to the client
         next(new Error("Authentication error: " + err.message));
     }
 };
