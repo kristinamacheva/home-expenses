56df1d905e1f57eb1c6478c4bae49e9b9dadd671
kristinamacheva <macheva.kristina@gmail.com>
2024-07-14 17:51:43 +0300

    Add allowances to the Household model

--- server/src/models/Household.js
+++ server/src/models/Household.js
@@ -24,8 +24,9 @@ const householdSchema = new mongoose.Schema({
             role: {
                 type: String,
                 enum: {
-                    values: ['Админ', 'Член', 'Дете'],
-                    message: 'Невалидна роля. Позволени стойности са "Админ", "Член" или "Дете".',
+                    values: ["Админ", "Член", "Дете"],
+                    message:
+                        'Невалидна роля. Позволени стойности са "Админ", "Член" или "Дете".',
                 },
                 required: [true, "Полето роля е задължително"],
             },
@@ -43,16 +44,26 @@ const householdSchema = new mongoose.Schema({
             user: {
                 type: mongoose.Types.ObjectId,
                 ref: "User",
-                required: [true, 'Полето потребител в баланса е задължително']
+                required: [true, "Полето потребител в баланса е задължително"],
             },
             sum: { type: Number, default: 0 },
             type: { type: String, enum: ["+", "-"], default: "+" },
         },
     ],
+    allowances: [
+        {
+            user: {
+                type: mongoose.Types.ObjectId,
+                ref: "User",
+                required: [true, "Полето потребител в джобни е задължително"],
+            },
+            sum: { type: Number, default: 0 },
+        },
+    ],
 });
 
 // Create index for frequently queried array fields
-householdSchema.index({ 'members.user': 1 });
+householdSchema.index({ "members.user": 1 });
 
 const Household = mongoose.model("Household", householdSchema);
 
