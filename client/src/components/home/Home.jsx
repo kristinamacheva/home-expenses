cee65c0d8bddd4e91addc20a7b58a14db807ccca
kristinamacheva <macheva.kristina@gmail.com>
2024-07-27 16:44:58 +0300

    Display allowances on home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -11,9 +11,11 @@ import { useEffect, useState, useContext } from "react";
 import * as authService from "../../services/authService";
 import BalanceListItem from "./balance-list-item/BalanceListItem";
 import AuthContext from "../../contexts/authContext";
+import AllowanceListItem from "./allowance-list-item/AllowanceListItem";
 
 export default function Home() {
     const [householdBalances, setHouseholdBalances] = useState([]);
+    const [householdAllowances, setHouseholdAllowances] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const { logoutHandler } = useContext(AuthContext);
 
@@ -21,6 +23,7 @@ export default function Home() {
 
     useEffect(() => {
         fetchHouseholdBalances();
+        fetchHouseholdAllowances();
     }, []);
 
     const fetchHouseholdBalances = () => {
@@ -40,6 +43,32 @@ export default function Home() {
                         title:
                             error.message ||
                             "Неуспешно зареждане на баланса на домакинствата",
+                        status: "error",
+                        duration: 6000,
+                        isClosable: true,
+                        position: "bottom",
+                    });
+                }
+            });
+    };
+
+    const fetchHouseholdAllowances = () => {
+        setIsLoading(true);
+        authService
+            .getHouseholdsWithExistingAllowances()
+            .then((result) => {
+                setHouseholdAllowances(result);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                if (error.status === 401) {
+                    logoutHandler();
+                } else {
+                    toast({
+                        title:
+                            error.message ||
+                            "Неуспешно зареждане на джобните на домакинствата",
                         status: "error",
                         duration: 6000,
                         isClosable: true,
@@ -77,11 +106,40 @@ export default function Home() {
                             ))}
                         </Flex>
                     ) : (
-                        <Flex
-                            justifyContent="center"
-                            alignItems="center"
-                        >
+                        <Flex justifyContent="center" alignItems="center">
                             <Text>Няма налични баланси</Text>
+                        </Flex>
+                    )}
+                </Stack>
+            </Stack>
+            <Stack mt={4}>
+                <Card background="white" p="2" boxShadow="xs">
+                    <HStack mx={4} my={2} alignItems="center" flexWrap="wrap">
+                        <Heading as="h1" size="lg" color="themePurple.800">
+                            Джобни
+                        </Heading>
+                    </HStack>
+                </Card>
+                <Stack mt="4" px="1">
+                    {isLoading ? (
+                        <Text ml="1">Зареждане...</Text>
+                    ) : householdAllowances.length > 0 ? (
+                        <Flex
+                            wrap="wrap"
+                            direction={{ base: "column", lg: "row" }}
+                            gap="4"
+                            align={{ base: "center", lg: "initial" }}
+                        >
+                            {householdAllowances.map((household) => (
+                                <AllowanceListItem
+                                    key={household._id}
+                                    household={household}
+                                />
+                            ))}
+                        </Flex>
+                    ) : (
+                        <Flex justifyContent="center" alignItems="center">
+                            <Text>Няма налични джобни</Text>
                         </Flex>
                     )}
                 </Stack>
