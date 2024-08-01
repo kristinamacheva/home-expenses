99a264bbe13f6bdcc91cc6959c7ff1cbcb618dfe
kristinamacheva <macheva.kristina@gmail.com>
2024-08-01 16:31:50 +0300

    Send lastMessageId if provided for cursor-based pagination

--- client/src/services/messageService.js
+++ client/src/services/messageService.js
@@ -3,8 +3,13 @@ import * as request from "../lib/request";
 const baseUrl = (householdId) =>
     `http://localhost:5000/households/${householdId}/messages`;
 
-export const getAll = async (householdId, page) => {
-    const url = `${baseUrl(householdId)}`;
+export const getAll = async (householdId, lastMessageId = null) => {
+    let url = `${baseUrl(householdId)}`;
+
+    // Use lastMessageId if provided for cursor-based pagination
+    if (lastMessageId) {
+        url += `?lastMessageId=${lastMessageId}`;
+    } 
 
     const result = await request.get(url);
     return result;
