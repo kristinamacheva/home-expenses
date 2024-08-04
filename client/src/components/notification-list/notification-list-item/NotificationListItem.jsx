752b708bd871514ff5dae58f9274b334030f0d43
kristinamacheva <macheva.kristina@gmail.com>
2024-08-04 18:53:29 +0300

    Add reminder route

--- client/src/components/notification-list/notification-list-item/NotificationListItem.jsx
+++ client/src/components/notification-list/notification-list-item/NotificationListItem.jsx
@@ -41,6 +41,9 @@ export default function NotificationListItem({ notification }) {
             case "Payment":
                 onOpenPaymentDetailsModal();
                 break;
+            case "Reminder":
+                navigate(`/reminders`);
+                break;
             default:
                 navigate(`/households/${notification.household}`);
                 break;
