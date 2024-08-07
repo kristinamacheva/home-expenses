c7d10d318aedaffaa5ea972f80c2581d128d951a
kristinamacheva <macheva.kristina@gmail.com>
2024-08-07 21:48:57 +0300

    Create ChildExpense model

---
+++ server/src/models/ChildExpense.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const childExpenseSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Полето 'Заглавие' е задължително"],
+        },
+        child: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+            required: [true, "Полето 'Дете' е задължително"],
+        },
+        household: {
+            type: mongoose.Types.ObjectId,
+            ref: "Household",
+            required: [true, "Полето 'Домакинство' е задължително"],
+        },
+        amount: {
+            type: Number,
+            required: [true, "Полето 'Сума' е задължително"],
+        },
+        date: { type: Date, required: [true, "Полето 'Дата' е задължително"] },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const ChildExpense = mongoose.model("ChildExpense", childExpenseSchema);
+
+module.exports = ChildExpense;
