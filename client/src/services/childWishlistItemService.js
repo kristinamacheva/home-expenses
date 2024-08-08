000ecca5f8fdb3d041085c26b0fa027639c529a2
kristinamacheva <macheva.kristina@gmail.com>
2024-08-08 23:09:46 +0300

    Create childWishlistItemService.js

---
+++ client/src/services/childWishlistItemService.js
@@ -0,0 +1,64 @@
+import * as request from "../lib/request";
+
+const baseUrl = (householdId) =>
+    `http://localhost:5000/households/${householdId}/childWishlistItems`;
+
+// params = {} -> handle optional parameters and prevent errors when accessing properties of params
+export const getAll = async (householdId, page, params = {}) => {
+    // Remove empty string parameters
+    Object.keys(params).forEach(
+        (key) => params[key] === "" && delete params[key]
+    );
+
+    const queryParams = new URLSearchParams({
+        page,
+        ...params,
+    });
+
+    const url = `${baseUrl(householdId)}?${queryParams.toString()}`;
+
+    const result = await request.get(url);
+    return result;
+};
+
+export const create = async (householdId, childWishlistItemData) => {
+    const url = baseUrl(householdId);
+    const result = await request.post(url, childWishlistItemData);
+
+    return result;
+};
+
+export const getOne = async (householdId, childWishlistItemId) => {
+    const url = baseUrl(householdId);
+    const result = await request.get(`${url}/${childWishlistItemId}`);
+
+    return result;
+};
+
+export const getEditableFields = async (householdId, childWishlistItemId) => {
+    const url = baseUrl(householdId);
+    const result = await request.get(
+        `${url}/${childWishlistItemId}?editable=true`
+    );
+
+    return result;
+};
+
+export const purchase = async (householdId, childWishlistItemId) => {
+    const url = baseUrl(householdId);
+    await request.put(`${url}/${childWishlistItemId}/purchase`);
+};
+
+export const edit = async (
+    householdId,
+    childWishlistItemId,
+    childWishlistItemData
+) => {
+    const url = baseUrl(householdId);
+    await request.put(`${url}/${childWishlistItemId}`, childWishlistItemData);
+};
+
+export const remove = async (householdId, childWishlistItemId) => {
+    const url = baseUrl(householdId);
+    await request.remove(`${url}/${childWishlistItemId}`);
+};
