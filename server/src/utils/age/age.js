d3a2e9de34ab5acd7b324aa69fc084c0bc6708e0
kristinamacheva <macheva.kristina@gmail.com>
2024-08-22 10:56:14 +0300

    Create util functions for age validation

---
+++ server/src/utils/age/age.js
@@ -0,0 +1,38 @@
+function isChildUnder14(birthdate) {
+    if (!birthdate) return false;
+
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    const age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    const dayDiff = today.getDate() - birthDate.getDate();
+
+    // Adjust if the birthdate has not occurred yet this year
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+
+    return age < 14;
+}
+
+function isChild18OrOver(birthdate) {
+    if (!birthdate) return false;
+
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    const age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    const dayDiff = today.getDate() - birthDate.getDate();
+
+    // Adjust if the birthdate has not occurred yet this year
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+
+    return age >= 18;
+}
+
+module.exports = {
+    isChildUnder14,
+    isChild18OrOver,
+};
